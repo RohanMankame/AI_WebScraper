13e649a3e485854aa6e8e9fc12c9dda093aadf13
Rohan Mankame <rohanmankame@outlook.com>
2025-10-15 22:01:40 +0530

    built web scraping component

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
+from scrape import scrape_site
+
 
 st.title("AI Web-Scraper")
 url = st.text_input("Enter a site URL you want scraped")
 
 if st.button("Scrape Site"):
     st.write("Scraping Website Now :D")
+    result = scrape_site(url)
+    print(result)
